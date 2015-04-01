c81aff6fa6cb923f5077170f19af5409ba4747c6
Manuel González García <leunam.san@gmail.com>
2015-04-01 19:20:41 +0200

    [Code] - Script + Anim + Graphics
    Room icon in green and red
    Animations for room icon
    New Graphics for room icon
    Script to control the room collider
    Animator States created.

---
+++ Code/Assets/Scripts/RoomController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomController : MonoBehaviour {
+
+    public GameObject room;
+    private Animator roomAnimator;
+	
+    
+    void Start () {
+        roomAnimator = room.GetComponent<Animator>();
+        roomAnimator.SetBool("Occupied", false); // Default Animator State - False.
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Patient")
+        {
+            roomAnimator.SetBool("Occupied", true);
+        }
+        //else if (other.tag = "Doctor") // JUST IN CASE
+        //{
+        //    roomAnimator.SetBool("Occupied", true);
+        //} 
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Patient")
+        {
+            roomAnimator.SetBool("Occupied", false);
+        }
+    }
+}
