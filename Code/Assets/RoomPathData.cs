ad098488b4fd88319cc1bd54fd9a08fc1e42c66b
Manuel González García <leunam.san@gmail.com>
2015-04-03 19:45:14 +0200

    [Code] - Patient find available rooms

---
+++ Code/Assets/RoomPathData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomPathData : MonoBehaviour {
+
+    public int patientsOnPath;
+
+	void Start () {
+        patientsOnPath = 0;
+	}
+
+    public int GetPatientsOnPath()
+    {
+        return patientsOnPath;
+    }
+
+    public void SetPatientsOnPath()
+    {
+        patientsOnPath++;
+    }
+	
+}
